d5ba0afdc10820a158eb0fbdbf212bbe5a4c9427
memesbay <memesbay@yahoo.com>
2022-11-27 01:15:54 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1026,13 +1026,15 @@ const VAULTABI = [
 var account = null;
 var contract = null;
 var vaultcontract = null;
+var stakeit = null;
+var unstakeit = null;
 
 const apikey = "DDRDSA7U9VHDDCQGJBZ218MYWRS21NYDS4";
 const NFTCONTRACT = "0x972b859685D4f5706632C36A98bdb796001072E7";
 const STAKINGCONTRACT = "0x0f928F5154358Ff4E921445790A64f0eACB382e5"
 const endpoint = "https://api.etherscan.io/api";
 const nftpng = "https://gateway.pinata.cloud/ipfs/QmWGBMo1uuKxypThVQwbfnYo2fHfVBCAWoGcKBQUyHR4BS/";
-const openseaapi = "https://api.opensea.io/api/v1/assets";
+const openseaapi = "https://opensea.io/collection/turtle-people-yatch-club";
 
 const providerOptions = {
 	binancechainwallet: {
