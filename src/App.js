885839e20adf26276d6f2515ad4405b624405036
memesbay <memesbay@yahoo.com>
2022-11-27 00:08:45 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1115,16 +1115,6 @@ class App extends Component {
 		contract.methods.mint(account, _mintAmount).send({ from: account, value: String(totalAmount) }); 
 	  } 
   
-  async function stakeit() {
-	  var tokenids = Number(document.querySelector("[name=stkid]").value);
-	  vaultcontract.methods.stake([tokenids]).send({from: account});
-  }
-  
-  async function unstakeit() {
-	  var tokenids = Number(document.querySelector("[name=stkid]").value);
-	  vaultcontract.methods.unstake([tokenids]).send({from: account});
-  }
-  
   async function claimit() {
 	  var tokenids = Number(document.querySelector("[name=claimid]").value);
 	  vaultcontract.methods.claim([tokenids]).send({from: account});
